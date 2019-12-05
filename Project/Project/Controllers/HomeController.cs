1ea3959153e99b76c944d351df09dac579459b60
swatichoudhry <55695298+swatichoudhry@users.noreply.github.com>
2019-12-05 14:10:22 +0530

    Project

---
+++ Project/Project/Controllers/HomeController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    { 
+  
+      ProjectEntities1 entity = new ProjectEntities1();
+
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+        public ActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            bool isValidUser = entity.Users.Any(u => u.username ==
+            user.username && u.u_password == user.u_password);
+
+            if (isValidUser)
+            {
+                    Session["username"] = user.username;             
+                System.Web.Security.FormsAuthentication.SetAuthCookie(user.username, false);
+                    //return View();
+                    return RedirectToAction("UserLogin", "Home");
+                }
+        }
+        ModelState.AddModelError("", "Invalid Username or Password");
+            return View();
+    }
+    public ActionResult LoginAdmin()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult LoginAdmin(Admin admin)
+    {
+        if (ModelState.IsValid)
+        {
+            bool isValidUser = entity.Admins.Any(u => u.admin_fullname ==
+            admin.admin_fullname && u.admin_password == admin.admin_password);
+
+            if (isValidUser)
+            {   
+                
+                System.Web.Security.FormsAuthentication.SetAuthCookie(admin.admin_fullname, false);             
+                return RedirectToAction("AdminLogin", "Home");
+            }
+        }
+        ModelState.AddModelError("", "Invalid Username or Password");
+        return View();
+    }
+        public ActionResult UserLogin()
+        {        
+            return View();
+        }
+
+        public ActionResult AdminLogin()
+        {
+            return View();
+            
+        }
+        [HttpPost]
+        public ActionResult AdminSport()
+        {
+           // TempData["Message"] = "You clicked Sports!";
+            return RedirectToAction("AdminSportTournament");
+        }
+        public ActionResult AdminSportTournament()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminSportTournament(Sport_tournamentlist add)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                entity.Sport_tournamentlist.Add(add);
+                entity.SaveChanges();
+                return RedirectToAction("AdminLogin", "Home");
+            }
+            ModelState.AddModelError("", "Invalid input");
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult AdminCultural()
+        {
+            //TempData["Message"] = "You clicked Culturals!";
+            return RedirectToAction("AdminCulturalEvent");
+        }
+        public ActionResult AdminCulturalEvent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminCulturalEvent(Cultural_eventlist add)
+        {
+                      
+            if (ModelState.IsValid)
+            {
+                entity.Cultural_eventlist.Add(add);
+                entity.SaveChanges();
+                return RedirectToAction("AdminLogin", "Home");
+            }
+            ModelState.AddModelError("", "Invalid input");
+            return View();
+
+        }
+
+       
+
+
+        public ActionResult Logout()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+            return RedirectToAction("SessionExpiry");
+        }
+        public ActionResult SessionExpiry()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project/Project/Controllers/SPORTSController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class SPORTSController : Controller
+    {
+        // GET: SPORTS
+        ProjectEntities1 entity = new ProjectEntities1();
+        public ActionResult Sports()
+        {
+            ViewBag.Sport = new SelectList(entity.Sport_tournamentlist, "sports_evid", "sport_name");
+
+            return View();
+
+        }
+        public ActionResult _mypartial(int Cid)
+        {
+            List<Sport_tournamentlist> modelll = entity.Sport_tournamentlist.Where(d => d.sports_evid == Cid).ToList();
+            ViewBag.ASQ = modelll;
+            ViewBag.PSP = modelll[0].sports_evid;
+            return PartialView("_mypartial");
+        }
+        public ActionResult addteam(int id)
+        {
+
+            Team team = new Team();
+
+            team.sp_id = id;
+            entity.Teams.Add(team);
+            entity.SaveChanges();
+            return RedirectToAction("Sports");
+        }
+
+
+        public ActionResult addini(int id)
+        {
+            Sports_reg sports_Reg = new Sports_reg();
+            var cd = Session["username"].ToString();
+            List<User> qw = entity.Users.Where(x => x.username == cd).ToList();
+            var ew = qw[0].uid;
+            if (entity.Sports_reg.Any(d => d.sport_id.Equals(id) && d.userid.Equals(ew)))
+            {
+                ViewBag.Message = "Can't Register for same sport twice";
+            }
+            else
+            {
+                sports_Reg.sport_id = id;
+                sports_Reg.userid = qw[0].uid;
+                var av = entity.Sport_tournamentlist.Find(id);
+                sports_Reg.sport_name = av.sport_name;
+                entity.Sports_reg.Add(sports_Reg);
+                entity.SaveChanges();
+                return RedirectToAction("Sports");
+            }
+            return View();
+        }
+
+
+        public ActionResult maketeams()
+        {
+            List<Sports_reg> li = entity.Sports_reg.Where(x => x.sport_id == 558).ToList();
+            int count = li.Count();
+            ViewBag.message = count;
+            int y = count / 11;
+            ViewBag.message1 = y;
+
+            //for(int i = 0; i < count; i++)
+            //{
+
+            //}
+            return View();
+        }
+
+
+    }
+}
