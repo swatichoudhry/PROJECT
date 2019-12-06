97cc1d6af643ae310d8ece30f0333b7a21e88f4e
swatichoudhry <55695298+swatichoudhry@users.noreply.github.com>
2019-12-06 16:22:26 +0530

    DXC .NET Training Project
    DXC Happenings includes sports and cultural events for which employees and view and register comfortably. Also the admin can monitor the events and employees' registration.

--- Project/Project/Controllers/CULTURALSController.cs
+++ Project/Project/Controllers/CULTURALSController.cs
@@ -13,40 +13,33 @@ namespace Project.Controllers
         ProjectEntities1 entity = new ProjectEntities1();
         public ActionResult Culturals()
         {
-            ViewBag.events = new SelectList(entity.Cultural_eventlist.ToList(), "event_name", "event_name");
-            //ViewBag.events = new SelectList(entity.Cultural_eventlist, "event_name", "cul_evid");
+            ViewBag.events = new SelectList(entity.Cultural_eventlist, "cul_evid", "event_name");
             return View();
         }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-
-        public ActionResult Culturals (Cultural_reg obj, FormCollection form)
-
+        public ActionResult addini(int id)
         {
-            var cd = Session["username"].ToString();
-            List<User> qw = entity.Users.Where(x => x.username == cd).ToList();          
-            obj.userid = qw[0].uid;
-            string event_name = form[1].ToString();
             Cultural_reg cultural_Reg = new Cultural_reg();
+            var cd = Session["username"].ToString();
+            List<User> qw = entity.Users.Where(x => x.username == cd).ToList();
+            var ew = qw[0].uid;
 
-            Cultural_eventlist cultural_Eventlist = entity.Cultural_eventlist.FirstOrDefault(x => x.event_name == event_name);
-            obj.eventid = cultural_Eventlist.cul_evid;
-            obj.event_name = cultural_Eventlist.event_name;
-
-
-
-            if (ModelState.IsValid)
+            if (entity.Cultural_reg.Any(d => d.cultural_id == id && d.userid == ew))
             {
-
-
-                entity.Cultural_reg.Add(obj);
+                ViewBag.Message = "Can't Register for same sport twice";
+            }
+            else
+            {
+                cultural_Reg.cultural_id = id;
+                cultural_Reg.userid = qw[0].uid;
+                var av = entity.Cultural_eventlist.Find(id);
+                cultural_Reg.event_name = av.event_name;
+                entity.Cultural_reg.Add(cultural_Reg);
+                ViewBag.Message = "Registered!";
                 entity.SaveChanges();
-                return RedirectToAction("Culturals");
             }
 
-            return View();
+            return PartialView("addini");
         }
-
 
         public ActionResult _mypartial(int Cid)
         {
